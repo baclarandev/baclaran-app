85f6bfa5652d33b01f7f4599c83216fe215d9b72
Baclaran Dev <baclarandev@gmail.com>
2026-02-03 10:49:38 +0800

    fix: fixed prisma error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import pkg from "pg";
-
-const { Pool } = pkg;
+import { Pool } from "pg";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
 const adapter = new PrismaPg(pool);
-
 export const prisma = new PrismaClient({ adapter });
