24de1c86310b429a795a25b50cc16ef2a2ff1515
Baclaran Dev <baclarandev@gmail.com>
2026-02-03 14:41:30 +0800

    fix: added a try catch block

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,13 @@ import { redirect } from "next/navigation";
 import { getSession } from "@/lib/auth";
 
 export default async function Home() {
-  const session = await getSession();
+  try {
+    const session = await getSession();
 
-  if (session) {
-    redirect("/dashboard");
-  } else {
-    redirect("/auth/login");
-  }
-  // return <div>hi</div>;
+    if (session) {
+      redirect("/dashboard");
+    }
+  } catch {}
+
+  redirect("/auth/login");
 }
