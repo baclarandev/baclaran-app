aa7cd24a6404518575d4d9b1ab6a4c046f188b1d
Baclaran Dev <baclarandev@gmail.com>
2026-02-03 11:23:37 +0800

    ref: downgrade to prisma v5

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,21 +1,11 @@
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
-import { Pool } from "pg";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
-const adapter = new PrismaPg(pool);
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma ||
+  globalForPrisma.prisma ??
   new PrismaClient({
-    adapter,
+    log: ["warn", "error"],
   });
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
